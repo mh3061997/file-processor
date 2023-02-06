5f2a1f6c4749a282deb7cdf0e4e82ec65081fd8d
Mohamed Haitham <mohamed.haitham@nexocraft.com>
2023-02-06 15:25:14 +0200

    Init Project

---
+++ src/test/java/com/haitham/fileprocessor/FileProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haitham.fileprocessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileProcessorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
